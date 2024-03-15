70941760e2136a633ecb7b18bcd8d031511b87dd
ahmedraza-developer <ahmed.r@crystallitedigital.com>
2024-03-15 22:32:24 +0500

    audio page complete

---
+++ audio-book-page/js/bookSlider.js
@@ -0,0 +1,31 @@
+$('.__bookSlider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+});
