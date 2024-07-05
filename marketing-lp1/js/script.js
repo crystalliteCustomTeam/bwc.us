39e127f0e7d79f9b5878126539a88893e4185720
ahmedraza-developer <ahmed.r@crystallitedigital.com>
2024-07-05 22:10:26 +0500

    lp content uploaded

--- marketing-lp1/js/script.js
+++ marketing-lp1/js/script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('.lpHeader');
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 200) {  
+        if (window.scrollY > 0) {  
             header.classList.add('scrolled')
         } else {
             header.classList.remove('scrolled');
